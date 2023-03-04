09153bb1d9eefe613c973a30497707c102e51525
Afra <afra.rahmanfard@gmail.com>
2023-03-03 19:42:07 -0700

    Progress

--- app/src/test/java/com/example/qrhunterapp_t11/QRCodeTest.java
+++ app/src/test/java/com/example/qrhunterapp_t11/QRCodeTest.java
@@ -1,15 +1,19 @@
 package com.example.qrhunterapp_t11;
 
 import static org.junit.jupiter.api.Assertions.*;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 public class QRCodeTest {
     private QRCode mockQR() {
         return new QRCode("696ce4dbd7bb57cbfe58b64f530f428b74999cb37e2ee60980490cd9552de3a6");
     }
+
     private QRCode mockQR2() {
         return new QRCode("555000500222ffff");
     }
+
     @Test
     public void testPoints() {
         QRCode qrCode = mockQR();
