44b434fc380997cb74648cf5b63d1be587300c0f
Sarah <sarah.michhelle@gmail.com>
2023-03-11 15:23:24 -0700

    wrote tests for QRcode methods calcpoints, uniquename, uniqueimage

--- app/src/test/java/com/example/qrhunterapp_t11/QRCodeTest.java
+++ app/src/test/java/com/example/qrhunterapp_t11/QRCodeTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 
 public class QRCodeTest {
     private QRCode mockQR(String valueString) {
@@ -17,11 +18,9 @@ public class QRCodeTest {
         int points;
 
         QRCode qrCode = mockQR("Test this string");
-        System.out.println(qrCode.getHash());
         Assertions.assertEquals(40, qrCode.getPoints());
 
         QRCode qrCode2 = mockQR("");
-        System.out.println(qrCode2.getHash());
         Assertions.assertEquals(28, qrCode2.getPoints());
 
         //test hash from eclass (Should be 115 points)
@@ -43,6 +42,79 @@ public class QRCodeTest {
         //test wrapping "hash"
         points = QRCode.calculatePoints("55558ee34121555");
         Assertions.assertEquals(164, points);
+
+    }
+
+    @Test
+    public void testName(){
+        String name;
+
+        QRCode qrCode = mockQR("Test this string");
+        Assertions.assertEquals("Old Boldicanmor", qrCode.getName());
+
+        QRCode qrCode2 = mockQR("");
+        Assertions.assertEquals("Young Boldomentas", qrCode2.getName());
+
+        //simple test to make sure the hash to binary works correctly
+        name = QRCode.uniqueName("080000");
+        Assertions.assertEquals("Big Largamengog", name);
+
+        //simple test to make sure the hash to binary works correctly
+        name = QRCode.uniqueName("07ffff");
+        Assertions.assertEquals("Old Sigsurolfli", name);
+
+    }
+
+    @Test
+    public void testImage(){
+        ArrayList<Integer> faceList;
+        ArrayList<Integer> expectedList = new ArrayList<>();
+
+        QRCode qrCode = mockQR("Test this string");
+        expectedList.add(2131165346);
+        expectedList.add(2131165344);
+        expectedList.add(2131165317);
+        expectedList.add(2131165428);
+        expectedList.add(2131165387);
+        expectedList.add(2131165347);
+
+        Assertions.assertEquals(expectedList, qrCode.getFaceList());
+
+        ArrayList<Integer> expectedList2 = new ArrayList<>();
+        expectedList2.add(2131165346);
+        expectedList2.add(2131165344);
+        expectedList2.add(2131165316);
+        expectedList2.add(2131165427);
+        expectedList2.add(2131165387);
+        expectedList2.add(2131165348);
+
+        QRCode qrCode2 = mockQR("");
+        System.out.println(qrCode.getHash());
+        Assertions.assertEquals(expectedList2, qrCode2.getFaceList());
+
+        //simple test to make sure the hash to binary works correctly
+        ArrayList<Integer> expectedList3 = new ArrayList<>();
+        expectedList3.add(2131165345);
+        expectedList3.add(2131165343);
+        expectedList3.add(2131165316);
+        expectedList3.add(2131165427);
+        expectedList3.add(2131165387);
+        expectedList3.add(2131165347);
+
+        faceList = QRCode.uniqueImage("080000");
+        Assertions.assertEquals(expectedList3, faceList);
+
+        //simple test to make sure the hash to binary works correctly
+        ArrayList<Integer> expectedList4 = new ArrayList<>();
+        expectedList4.add(2131165346);
+        expectedList4.add(2131165344);
+        expectedList4.add(2131165317);
+        expectedList4.add(2131165428);
+        expectedList4.add(2131165388);
+        expectedList4.add(2131165348);
+
+        faceList = QRCode.uniqueImage("07ffff");
+        Assertions.assertEquals(expectedList4, faceList);
 
     }
 
