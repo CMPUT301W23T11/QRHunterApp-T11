ade0d0f0e7a8297dcea7157daebd4566cde04945
apollo-online <alynch1@ualberta.ca>
2023-03-24 23:06:58 -0600

    New splashscreen

---
+++ app/src/main/java/com/example/qrhunterapp_t11/activities/SplashActivity.java
@@ -0,0 +1,43 @@
+package com.example.qrhunterapp_t11.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.example.qrhunterapp_t11.R;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.TextView;
+
+
+/**
+ * Handles logic for showing a splashscreen (the built-in android splashscreen library sucks).
+ *
+ * @referece Coding With Tea - https://www.youtube.com/watch?v=VcrzLcokvvc- how to implement the logic and layout of the splashscreen. Used without major modification.
+ * @reference lottieicon - https://lottiefiles.com/78072-map-pin-location - created the map loading animation.
+ */
+public class SplashActivity extends AppCompatActivity {
+
+    TextView appName;
+    LottieAnimationView mapAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        appName = findViewById(R.id.app_name);
+        mapAnimation = findViewById(R.id.splash_animation);
+
+        appName.animate().translationY(-600).setDuration(2100).setStartDelay(0);
+        mapAnimation.animate().translationY(-300).setDuration(1700).setStartDelay(0);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        }, 4500);
+    }
+}
