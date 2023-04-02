02fef3b710d09c410d6b6b657c9647596564b757
Afra <afra.rahmanfard@gmail.com>
2023-04-02 03:12:32 -0600

    Working on regional leaderboard, intent tests

--- app/src/androidTest/java/com/example/qrhunterapp_t11/SettingsFragmentTest.java
+++ app/src/androidTest/java/com/example/qrhunterapp_t11/SettingsFragmentTest.java
@@ -15,9 +15,12 @@ import androidx.test.rule.ActivityTestRule;
 
 import com.example.qrhunterapp_t11.activities.MainActivity;
 import com.example.qrhunterapp_t11.interfaces.QueryCallback;
+import com.example.qrhunterapp_t11.objectclasses.QRCode;
 import com.example.qrhunterapp_t11.objectclasses.User;
 import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.robotium.solo.Solo;
 
 import org.junit.After;
@@ -25,6 +28,7 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.Random;
 
 /**
@@ -37,11 +41,12 @@ public class SettingsFragmentTest {
 
     private final FirebaseFirestore db = FirebaseFirestore.getInstance();
     private final CollectionReference usersReference = db.collection("Users");
+    private final CollectionReference qrCodesReference = db.collection("QRCodes");
     private final Random rand = new Random();
     private final String testUsername = "testUser" + rand.nextInt(1000000);
     private Solo solo;
-    private String username;
     private SharedPreferences prefs;
+    private String username;
     @Rule
     public ActivityTestRule<MainActivity> rule = new ActivityTestRule<MainActivity>(MainActivity.class) {
 
@@ -120,12 +125,32 @@ public class SettingsFragmentTest {
     @Test
     public void testUsernameChange() {
         String testUserUnique = "testUserUnique";
+
         // Make sure testUserUnique displayName is unique
         checkUniqueDisplayName(testUserUnique, new QueryCallback() {
             public void queryCompleteCheck(boolean unique) {
                 assertTrue(unique);
+
             }
         });
+
+        QRCode qrCode1 = mockQRCode();
+        QRCode qrCode2 = mockQRCode();
+
+        for (int i = 0; i < 2; i++) {
+            HashMap<String, String> comment = new HashMap<>();
+            comment.put("commentString", "test comment" + i);
+            comment.put("displayName", testUsername);
+            comment.put("username", testUsername);
+            qrCodesReference.document(qrCode1.getID()).collection("commentList").add(comment);
+
+            comment.put("commentString", "test comment" + (3 * i));
+            qrCodesReference.document(qrCode2.getID()).collection("commentList").add(comment);
+
+            comment.put("username", "falseTestUser");
+            qrCodesReference.document(qrCode1.getID()).collection("commentList").add(comment);
+            qrCodesReference.document(qrCode2.getID()).collection("commentList").add(comment);
+        }
 
         solo.clickOnView(solo.getView(R.id.settings));
 
@@ -142,13 +167,17 @@ public class SettingsFragmentTest {
         // Just a friendly, pleasant conversation.
 
         // Make sure displayName was successfully changed
-        usersReference.document(prefs.getString("currentUserUsername", null))
-                .get()
-                .addOnSuccessListener(user -> {
-                    assertEquals(user.get("displayName"), username);
-                    usersReference.document(testUserUnique).delete();
-                });
+        checkDisplayNameChanged(qrCode1, qrCode2, new QueryCallback() {
+            @Override
+            public void queryCompleteCheck(boolean queryComplete) {
+                assert (queryComplete);
+                deleteMockQRCode(qrCode1);
+                deleteMockQRCode(qrCode2);
+                usersReference.document(testUserUnique).delete();
+            }
+        });
     }
+
 
     /**
      * Adds a test user to the database.
@@ -172,5 +201,62 @@ public class SettingsFragmentTest {
                 .addOnSuccessListener(results -> {
                     uniqueUsername.queryCompleteCheck(results.isEmpty());
                 });
+    }
+
+    /**
+     * Checks if the given displayName was correctly changed
+     */
+    public void checkDisplayNameChanged(QRCode qrCode1, QRCode qrCode2, final QueryCallback queryComplete) {
+        usersReference.document(prefs.getString("currentUserUsername", null))
+                .get()
+                .addOnSuccessListener(user -> {
+                    assertEquals(user.get("displayName"), prefs.getString("currentUserDisplayName", null));
+
+                    qrCodesReference.document(qrCode1.getID()).collection("commentList")
+                            .get()
+                            .addOnSuccessListener(qrCode1Comments -> {
+                                for (QueryDocumentSnapshot comment : qrCode1Comments) {
+                                    System.out.println(comment.getData());
+                                }
+
+                                qrCodesReference.document(qrCode2.getID()).collection("commentList")
+                                        .get()
+                                        .addOnSuccessListener(qrCode2Comments -> {
+                                            for (QueryDocumentSnapshot comment : qrCode2Comments) {
+                                                System.out.println(comment.getData());
+                                            }
+
+                                            queryComplete.queryCompleteCheck(true);
+                                        });
+                            });
+                });
+    }
+
+    /**
+     * Create a mock QR Code
+     *
+     * @return QRCode object
+     */
+    private QRCode mockQRCode() {
+        final int randomNum = new Random().nextInt(10000);
+        QRCode qrCode = new QRCode("test" + randomNum);
+
+        qrCodesReference.document(qrCode.getID()).set(qrCode);
+        DocumentReference qrCodeReference = db.collection("QRCodes").document(qrCode.getID());
+        HashMap<String, DocumentReference> qrReferenceMap = new HashMap<>();
+        qrReferenceMap.put("Reference", qrCodeReference);
+        usersReference.document(testUsername).collection("User QR Codes").document(qrCode.getHash()).set(qrReferenceMap);
+
+        return qrCode;
+    }
+
+    /**
+     * Delete mock QR Code
+     *
+     * @param qrCode QRCode object to delete
+     */
+    private void deleteMockQRCode(QRCode qrCode) {
+        qrCodesReference.document(qrCode.getID()).delete();
+        usersReference.document(testUsername).collection("User QR Codes").document(qrCode.getID()).delete();
     }
 }
